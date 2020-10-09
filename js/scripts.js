ffabb8998aa33792f9bc11c2a14f2428ee490291
EveNyarango <evenyarango@gmail.com>
2020-10-09 11:53:48 +0300

    add a jquery function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $("#design").click (function(){
         $("#par1").toggle("slow");
-
     });
 
+    $("#dev").click (function(){
+        $("#par2").toggle("slow");
+
+    });$("#product").click (function(){
+        $("#par3").toggle("slow");
+    });
 });
